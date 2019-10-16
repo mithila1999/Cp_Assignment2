a60b5dd2d10771cc1670740242d75f660f613675
mithila1999 <56624202+mithila1999@users.noreply.github.com>
2019-10-16 11:55:26 +0530

    Update q_1.c

--- q_1.c
+++ q_1.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-void main()
+int main()
 {
 	int i,j,k;
 	for(i=1;i<=5;i++)
 	{
-		for(k=1;k<i;k++)
+		for(j=1;j<i;j++)
 		{
 		printf(" ");
 		}
-		for(j=5;j>=i;j--)
+		for(k=5;k>=i;k--)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}
+	return 0;
 }
